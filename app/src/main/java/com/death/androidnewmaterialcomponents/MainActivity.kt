a5202a8c331c02d91a3aff0c7d5102caa269a5b1
deathcode <carotkut12@gmail.com>
2019-01-05 05:57:00 -0800

    added another activity:

--- app/src/main/java/com/death/androidnewmaterialcomponents/MainActivity.kt
+++ app/src/main/java/com/death/androidnewmaterialcomponents/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.death.androidnewmaterialcomponents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab.setOnClickListener {
+            startActivity(Intent(this,OtherComponent::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/death/androidnewmaterialcomponents/OtherComponent.kt
@@ -0,0 +1,12 @@
+package com.death.androidnewmaterialcomponents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherComponent : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_component)
+    }
+}
